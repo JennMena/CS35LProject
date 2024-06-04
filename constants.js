4dd3c2a4eb197aa0740f71f8fdabec29d5144adf
samyurs <samyurs07@g.ucla.edu>
2024-06-03 23:00:54 -0700

    Edited Homepage formatting

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 // Update with your credentials
 const DB_USER = 'sa';
-const DB_PASSWORD = '3950Spork!';
+const DB_PASSWORD = 'YourStrong@Password';
 const DB_SERVER = 'localhost';
 const DB_DATABASE = 'BUDGET';
 
